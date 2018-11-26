e5d7f2c3642811fd406c90a1eb73987412e85aad
eljust <rubenmanza98@gmail.com>
2018-11-26 19:33:40 +0100

    acabant

---
+++ Sprint 2/models/conexio.php
@@ -0,0 +1,7 @@
+<?php
+$connexio=array(
+    "servidor"=>"127.0.0.1",
+    "bd"=>"concesionario", 
+    "usuari"=>"root", 
+    "contrassenya"=>"");
+?>
---
+++ Sprint 2/models/usuari.php
@@ -0,0 +1,26 @@
+<?php
+class Usuari{
+    
+    public function validaUsuari($usuari, $pass){
+        
+        try{
+            require_once "connexio.php";
+            $cadenaConnexio="mysql:host=".$connexio["servidor"].";dbname=".$connexio['bd'];
+            $db = new PDO($cadenaConnexio, $connexio["usuari"], $connexio["contrassenya"]); 
+            $consulta = $db->prepare('SELECT rol FROM Usuari WHERE login = ? AND Password = ?');
+        
+            $consulta->execute(array($usuari, $pass));
+            
+            $role=false;
+            while($fila=$consulta->fetch()){
+                $role=$fila[0];
+            }
+            $db=null;
+            return $role;
+        } catch (Exception $e){
+            echo("Error:".$e->getMessage());
+            $db=null;
+        }
+    }
+}
+
